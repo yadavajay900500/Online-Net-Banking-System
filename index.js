8d081f080e3984ed8bc03b7798c614e9a5dde1a2
yadavajay900500 <yadavajay900500@gmail.com>
2022-09-20 16:27:42 +0530

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const server = require("./server");
-const PORT = 2022;
+const PORT =process.env.PORT ||  2022;
 
 server.listen(PORT,()=> {
     console.log(`Server Started on http://localhost:${PORT}`, );
